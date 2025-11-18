4ac43961e4acb48a23391e5f9976d862f10b0913
Caleb Johnson-Cantrell <46861012+calebrjc@users.noreply.github.com>
2025-11-17 22:51:11 -0500

    feat: Add oh-my-posh; format ZSH config files

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,6 +7,10 @@ config.font = wezterm.font("GoogleSansCode Nerd Font Mono")
 config.color_scheme = "Catppuccin Mocha"
 config.initial_rows = 30
 config.initial_cols = 120
+config.default_cursor_style = "BlinkingUnderline"
+config.animation_fps = 1
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
 -- Window
 config.enable_wayland = false
